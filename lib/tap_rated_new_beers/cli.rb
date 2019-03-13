74d60183894e7b5caf435acfbb670b325127f1ca
Minajeanne <albrown07@mac.com>
2019-03-13 15:21:06 -0400

    Added #select_beers

--- lib/tap_rated_new_beers/cli.rb
+++ lib/tap_rated_new_beers/cli.rb
@@ -2,35 +2,66 @@
 class TapRatedNewBeers::CLI
 
   def call
-    puts "|_|Top Rated Beers:New|_|"
     list_beers
     select_beer
+    exit_cli
   end
 
-  # def start
-  #   puts "Welcome to..."
-  #   # put scraper here
-  #   # put menu here
-  # end
-
   def list_beers
+    puts "Please wait while we brew your beers..."
+    sleep(4)
+    puts "Top Rated Beers: New"
     # list beers here
     puts "1...25"
   end
 
   def select_beer
-    puts "Select the number of the beer you'd like to sample:"
+    puts ""
+    # input = nil
+    # while input != 'exit'
+    puts "Select the number of the beer you'd like to sample or type 'exit':"
+      input = gets.strip.downcase
+
+    beer = TapRatedNewBeers::Beer.find(input.to_i)
+
+    print_beers(beer)
+
+    puts ""
+    puts "Would you like to sample another beer? Y or N"
+
+      input = gets.strip.downcase
+        if input == "y"
+          select_beer
+        elsif input == "n"
+          puts ""
+          puts "Happy Drinking!"
+          exit_cli
+        else
+          puts ""
+          puts "Have you been drinking? Please try again."
+          select_beer
+         end
+  end
+
+  #     case input
+  #     when "1"
+  #       puts "Beer 1"
+  #     when "2"
+  #       puts "Beer 2"
+  #     when "list"
+  #       list_beers
+  #     end
+  #   end
+  # end
+
+  def exit_cli
+    puts "Happy Drinking!"
+  end
+
+  def print_beers(beer)
+    
   end
 
 
-
-
 # Title Bar = (.mainContainer .titleBar).text
-
-
-
-
-
-
-
 end
